e015dcf6def4f181a636ad8795b4d4d0a1442bd3
bildovich <bildovich@yandex.ru>
2021-05-09 14:14:29 +1000

    Чистый проект

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'ООО Торговый дом',
+    }
+  },
+}
+</script>
